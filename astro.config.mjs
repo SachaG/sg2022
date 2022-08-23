ca6a511ddc3360ecb88bd8284d704b1fc72d1993
Sacha Greif <SachaG@users.noreply.github.com>
2022-08-23 15:09:58 +0900

    port 2017 Gatsby site to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
+import ViteYaml from '@modyfi/vite-plugin-yaml';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  vite: {
+    // Example: Add custom vite plugins directly to your Astro project
+    plugins: [ViteYaml()],
+  }
+});
